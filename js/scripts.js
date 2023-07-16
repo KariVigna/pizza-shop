947c31aa192fcb52295a0f8cc7b8e6d6d05a442b
Kari Vigna <karivigna24@gmail.com>
2023-07-15 17:44:35 -1000

    topping cost prototype created

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
-function Pizza(toppings: [], size)
+function Pizza(toppings, size)
     this.toppings = [];
     this.size = 0;
 
-Pizza.prototype.calculateCost = 
+Pizza.prototype.calculateToppingCost = 
+toppings.forEach(function(topping) {
+let toppingInput = document.querySelectorAll("input[name=topping]:checked");
+toppingInput.push(this.toppings)
+let toppingPrice = toppings * 3
+return toppingPrice
+});
+
+Pizza.prototype.caculateSizeCost = function(size) {
+    sizeSelect = document.querySelector("#size");
+    if 
+
+    if 
+
+}
+
+
+
+
+//user interface
+
