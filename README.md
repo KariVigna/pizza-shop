057fabe1d028d0bf1b851a35e341e58ac77f892b
Kari Vigna <karivigna24@gmail.com>
2023-07-17 08:39:31 -0700

    uses a more simple fuction to calculate topping cost

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Test: It should return an object with a toppings property and a size property.
 Code: Pizza()
 Expected Output: Pizza (toppings:[], size)
 
+Describe: Pizza.prototype.calculateToppingCost
+
+Test: It should return a new variable containing 
+
--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,24 @@ function Pizza(toppings, size) {
     this.size = 0;
 }
 
-Pizza.prototype.calculateToppingCost = 
-toppings.forEach(function(topping) {
-let toppingInput = document.querySelectorAll("input[name=topping]:checked");
-toppingInput.push(this.toppings)
-let toppingPrice = toppings * 3
-return toppingPrice
-});
+Pizza.prototype.calculateToppingCost = function(topping) {
+    let toppingCost = this.toppings.length * 3;
+    return toppingCost;
+};
 
-Pizza.prototype.caculateSizeCost = function(size) {
-    sizeSelect = document.querySelector("#size");
+let myPizza = new Pizza(["pepperoni", "onion", "pineapple"], med)
 
-}
+// Pizza.prototype.calculateToppingCost = 
+// toppings.forEach(function(topping) {
+// let toppingInput = document.querySelectorAll("input[name=topping]:checked");
+// toppingInput.push(this.toppings)
+// let toppingPrice = toppings * 3
+// return toppingPrice
+// });
+
+// Pizza.prototype.caculateSizeCost = function(size) {
+//     sizeSelect = document.querySelector("#size");
+// }
 
 
 //user interface
